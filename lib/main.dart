372877a57e88cbc7560db334c2dbf7c4326fc99b
jeffsui <pinghailinfeng79@gmail.com>
2019-11-25 17:33:07 +0800

    登录页面,添加路由

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myapp/navigator/First_Screen.dart';
 import 'package:myapp/navigator/navigation_home.dart';
 import 'package:myapp/navigator/todo_screen.dart';
+import 'package:myapp/pages/login/login_demo.dart';
 import 'mytabs.dart';
 import 'layout/row_demo.dart';
 import 'model/todo.dart';
@@ -18,9 +19,11 @@ class MyApp extends StatelessWidget{
       // home:NavigationHomeScreen(),
       theme: new ThemeData(
         primaryColor: Colors.blue,
+        backgroundColor: Colors.lightBlue,
       ),
-      initialRoute: '/row',
+      initialRoute: '/login',
       routes: <String,WidgetBuilder>{
+        '/login':(BuildContext context)=> LoginDemo(),
         '/tabs': (BuildContext context) => Tabs(),
         '/row':(BuildContext context) => RowDemo(),
         '/navigation':(BuildContext context) =>NavigationHomeScreen(),
